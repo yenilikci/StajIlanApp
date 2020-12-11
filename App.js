0a2901efb1ddbf76e673eca8ec7766eeb7922863
Melih Çelik <57464067+yenilikci@users.noreply.github.com>
2020-12-11 15:15:27 +0300

    Components klasörü eklendi ~ AppBar bileşeni yazıldı

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { Text, View, StatusBar } from 'react-native';
+import { Text, View, StatusBar, SafeAreaView } from 'react-native';
+import AppBar from './components/AppBar';
+
+const Container = SafeAreaView;
 
 const App = () => {
   return (
     <>
     <StatusBar/>
+    <Container>
+    <AppBar/>
+    </Container>
     </>
   );
 }
