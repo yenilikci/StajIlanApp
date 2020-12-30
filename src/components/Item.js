4f055f0f35c5febcbe06e000d1b4f5128d0eff1f
Melih Çelik <57464067+yenilikci@users.noreply.github.com>
2020-12-30 17:21:48 +0300

    Axios Kütüphanesi Dahil Edildi, İlan Bilgilerini Kullanacağım Item Component Oluşturuldu

---
+++ src/components/Item.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {Text, View} from "react-native";
+
+const Item = ({item}) =>{
+    return(
+    <View>
+    </View>
+    )
+};
+
+export default Item;
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,25 +1,53 @@
 import React from "react";
-import { View, Button, Text, StyleSheet } from "react-native";
-import AppBar from "../components/AppBar";
+import { View, Button, Text, StyleSheet,FlatList } from "react-native";
+import axios from 'axios';
+import Item from "../components/Item";
 
-const Home = ({ navigation }) => {
-    return (
-        <View style={styles.center}>
-            <Text>Anasayfa</Text>
-            <Button
-                title="Hakkında Sayfasına Git"
-                onPress={() => navigation.navigate("Hakkında")}
-            />
-        </View>
-    );
-};
+class Home extends React.Component
+{
+    constructor(props) {
+        super(props);
+        this.state = {
+            data:[]
+        }
+    }
+
+
+    componentDidMount(){
+        axios.get(`https://stajilan.com/api/ads/`)
+            .then((res) => {
+                this.setState({data:res.data})
+            })
+            .catch((e) => log(e))
+    }
+
+    _renderİtem = ({item}) => {
+        return <Item item={item}/>
+    }
+
+    render() {
+        const {data} = this.state;
+        return (
+            <View style={styles.center}>
+                <Text>Anasayfa</Text>
+                <Button
+                    title="Hakkında Sayfasına Git"
+                    onPress={() => this.props.navigation.navigate("Hakkında")}
+                />
+                <FlatList
+                    data={data}
+                    numColumns = {5}
+                    keyExtreactor = {(item) => item.id.toString()}
+                    renderItem={this._renderİtem}
+                />
+            </View>
+        );
+    }
+}
 
 const styles = StyleSheet.create({
     center: {
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        textAlign: "center",
     },
 });
 
